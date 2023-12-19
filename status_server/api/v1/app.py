a7a4839ae92df6e6090527f308dccb3882971f70
SPaci20 <spaci20@gmail.com>
2023-12-19 15:52:35 -0800

    Debugging 1

--- status_server/api/v1/app.py
+++ status_server/api/v1/app.py
@@ -6,15 +6,11 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
 
-
-@app.errorhandler(404)
-def not_found(error):
-    """ json 404 page """
-    return make_response(jsonify({"error": "Not found"}), 404)
-
+# Define the route for the status
+@app.route('/api/v1/status', methods=['GET'])
+def get_status():
+    return jsonify({"status": "API is up and running"})
 
 if __name__ == "__main__":
-    # python -m api.v1.app 
     app.run(host="0.0.0.0", port=5000)
